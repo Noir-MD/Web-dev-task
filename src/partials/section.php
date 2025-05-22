8ec629d559579dc526be370162782199463bfe37
Nix <atpengineer8484@gmail.com>
2025-05-23 05:29:07 +0700

    Add files via upload

--- src/partials/section.php
+++ src/partials/section.php
@@ -2,6 +2,6 @@
 // filepath: src/partials/section.php
 ?>
 <section class="content-box mb-4">
-  <h2><?= htmlspecialchars($title) ?></h2>
+  <h2 class="text-primary"><?= htmlspecialchars($title) ?></h2>
   <?= $content ?>
 </section>
