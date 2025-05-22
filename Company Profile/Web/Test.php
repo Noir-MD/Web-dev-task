6c5d30d731670cd8d852e255a3445e623688ea87
Nix <atpengineer8484@gmail.com>
2025-05-22 23:38:06 +0700

    Add files via upload

---
+++ Company Profile/Web/Test.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Webpage</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../src/main.css">
+    <script src="../asset/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="text-bg-primary p-3">Primary with contrasting color</div>
+    <div class="text-bg-secondary p-3">Secondary with contrasting color</div>
+    <div class="text-bg-success p-3">Success with contrasting color</div>
+    <div class="text-bg-danger p-3">Danger with contrasting color</div>
+    <div class="text-bg-warning p-3">Warning with contrasting color</div>
+    <div class="text-bg-info p-3">Info with contrasting color</div>
+    <div class="text-bg-light p-3">Light with contrasting color</div>
+    <div class="text-bg-dark p-3">Dark with contrasting color</div>
+
+    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="Images/british 1.jpeg" class="d-block w-100 rounded shadow" alt="british 1" style="max-width:220px; max-height:180px; object-fit:contain; background:#fff;">
+    </div>
+    <div class="carousel-item">
+      <img src="Images/british 2.jpeg" class="d-block w-100 rounded shadow" alt="british 2" style="max-width:220px; max-height:180px; object-fit:contain; background:#fff;">
+    </div>
+    <div class="carousel-item">
+      <img src="Images/british 3.jpeg" class="d-block w-100 rounded shadow" alt="british 3" style="max-width:220px; max-height:180px; object-fit:contain; background:#fff;">
+    </div>
+  </div>
+</div>
+</body>
+</html>
